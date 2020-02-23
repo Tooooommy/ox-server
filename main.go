9ad1fc7e021e63ed48c65d0ddb3a232e9528e9a1
tooooommy <tommy_mgt@163.com>
2020-02-24 00:20:59 +0800

    websocket: upgrade and connection

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
-import "net"
+import (
+	"ox-web/core"
+)
 
 // 一个类似gin的框架
 // 路由使用前缀树
 func main() {
-	//app := core.New()
-	//app.GET("/", func(context *core.Context) {
-	//	context.JSON(200, core.M{
-	//		"message": "go",
-	//	})
-	//})
-	//app.Run(":8080")
-	net.Dial()
+	app := core.New()
+	app.GET("/", func(context *core.Context) {
+		context.JSON(200, core.M{
+			"message": "go",
+		})
+	})
+	_ = app.Run(":8080")
 }
 
 //func Upgrade(writer http.ResponseWriter, request *http.Request) error {
